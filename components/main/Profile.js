e8ca638095338760f6f7aad9ad7a7b99e0071a26
Ron Lavit <lavitr01051977@gmail.com>
2021-02-22 11:07:58 +0300

    Profile implementation

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,23 +1,65 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image, FlatList } from "react-native";
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 
 export function Profile(props) {
-	const {currentUser, posts} = props
+	const { currentUser, posts } = props
 	console.log('snapshot', posts)
 	console.log('currentUser', currentUser)
 
 	return (
-		<View>
-			<Text>Profile</Text>
+		<View style={styles.container}>
+			<View style={styles.containerInfo}>
+				<Text>{currentUser.name}</Text>
+				<Text>{currentUser.email}</Text>
+			</View>
+			<View style={styles.containerGalery}>
+				<FlatList
+					numColumns={3}
+					horizontal={false}
+					data={posts}
+					renderItem={
+						({ item }) => (
+							<View style={styles.containerImage}>
+
+								<Image
+									style={styles.image}
+									source={{ uri: item.getDownloadURL }}
+								/>
+							</View>
+						)
+					}
+				/>
+			</View>
 		</View>
 	);
 }
 
-const mapStateToProps = (store)=> ({
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginTop: 40
+	},
+	containerInfo: {
+		margin: 20
+	},
+	containerGalery: {
+
+	},
+	image: {
+		flex: 1,
+		aspectRatio: 1/1
+	},
+	containerImage: {
+		flex: 1/3
+	}
+
+})
+	
+const mapStateToProps = (store) => ({
 	currentUser: store.userState.currentUser,
 	posts: store.userState.posts
 })
 
-export default  connect(mapStateToProps, null)(Profile)
+export default connect(mapStateToProps, null)(Profile)
